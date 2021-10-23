d69b60c81888e294a4d2e338c1e7336f35ada053
Michael Wang <mh5wang@edu.uwaterloo.ca>
2021-10-23 12:56:04 -0400

    Corrected typo in comments

--- Q24_lexicographic_permutations.py
+++ Q24_lexicographic_permutations.py
@@ -1,7 +1,7 @@
 def main():
     from itertools import permutations
     # Use permutations function of itertools to generate all permutations of digits 0-9, no need to sort since
-    # permutations automatically puts entries in lexicographical order, put results into list, get index 999999
+    # permutations automatically puts entries in lexicographical order, put results into list, get index 999'999
     # (i.e. the millionth entry since list is 0 indexed), use join to put list into string and print
     print(''.join(list(permutations(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 10))[999999]))
 
