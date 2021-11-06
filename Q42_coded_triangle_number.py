0f60a3f03b2981a8213c1d092d0f9100277ab41b
Michael Wang <mh5wang@edu.uwaterloo.ca>
2021-11-05 22:02:57 -0400

    Implemented Q42 using a generator function and HTTP request

--- Q42_coded_triangle_number.py
+++ Q42_coded_triangle_number.py
@@ -1,3 +1,5 @@
+import requests
+
 # generator function that yields triangle numbers
 def triangle_numbers():
     n = 1
@@ -16,22 +18,22 @@ def main():
     triangle_counter = 0  # number of words with triangle number values
     triangle_list = [1]  # list of triangle numbers
     tri_gen = triangle_numbers()  # generator function that yields triangle numbers
-    with open('external_files/Q42_words.txt', 'r') as f:
-        # read in the file, remove the double quotes and make into a list by splitting on commas
-        words = f.read().replace('\"', '').split(',')
-        for word in words:
-            value = word_value(word)
-            # if the value of the current word is larger than the largest triangle number in the list
-            if value > max(triangle_list):
-                # while the largest triangle number in the list is less than the current value
-                while max(triangle_list) < value:
-                    # append triangle numbers to the list using the generator
-                    triangle_list.append(next(tri_gen))
-            # try to find the value in the list of triangle numbers
-            if value in triangle_list:
-                triangle_counter += 1
+    url = 'https://projecteuler.net/project/resources/p042_words.txt'
+    # get the words from the file on the project Euler website, remove the quotations and split into list on commas
+    words = requests.get(url, allow_redirects=True).text.replace("\"", "").split(',')
 
-    f.close()
+    for word in words:
+        value = word_value(word)
+        # if the value of the current word is larger than the largest triangle number in the list
+        if value > max(triangle_list):
+            # while the largest triangle number in the list is less than the current value
+            while max(triangle_list) < value:
+                # append triangle numbers to the list using the generator
+                triangle_list.append(next(tri_gen))
+        # try to find the value in the list of triangle numbers
+        if value in triangle_list:
+            triangle_counter += 1
+
     print(triangle_counter)
 
 
