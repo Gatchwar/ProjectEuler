ffd5fa230dcf7b12e7092ae146dfaaf8443fd825
Michael Wang <mh5wang@edu.uwaterloo.ca>
2021-11-26 23:41:08 -0500

    Improved Q58

--- Q58_spiral_primes.py
+++ Q58_spiral_primes.py
@@ -2,16 +2,15 @@ from euler_module.euler_functions import is_prime
 
 
 def main():
-    n = 170000000
-    # primes = prime_gen(n)
-
     done = False
     skip = 2  # how many numbers ahead is the next diagonal
     prime_counter = 0
     diagonal_counter = 1
     current = 1  # current number in the spiral
     side_length = 1
+    # use done boolean to end loop since it is not known how many iterations this will take
     while not done:
+        # for every corner in the current layer
         for corner in range(4):
             current += skip
             if is_prime(current):
